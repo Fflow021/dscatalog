b45e802482e3be12162e0cd7f7521142b3eced8b
Phillipe Wolff <phillipe.wolff@gmail.com>
2023-09-14 21:35:33 -0300

    seeding and product entity

---
+++ src/main/java/com/devsuperior/dscatalog/resources/ProductResources.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.resources;
+
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResources {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
---
+++ src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+}
