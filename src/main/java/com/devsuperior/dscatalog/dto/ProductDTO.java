22090406dc7b4e35528ff5f0e65908bb5b215de9
Phillipe Wolff <phillipe.wolff@gmail.com>
2023-09-20 17:29:31 -0300

    exception handling

---
+++ src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,86 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ProductDTO implements Serializable {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgURL;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgURL = entity.getImgURL();
+    }
+
+    public ProductDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductDTO that = (ProductDTO) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
