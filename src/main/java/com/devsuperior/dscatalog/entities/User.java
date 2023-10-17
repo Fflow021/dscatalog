a84374ac61ee6e362b8fb566000e69e390506c66
Phillipe Wolff <phillipe.wolff@gmail.com>
2023-10-17 04:54:30 -0300

    User CRUD start

--- src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -18,7 +18,7 @@ public class User implements Serializable {
     private String lastName;
     private String email;
     private String password;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -72,6 +72,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 
     @Override
